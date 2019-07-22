4b34c0b9a21db5a2d49a5f338cf194e2964d3c09
Eric Mauldin <emauldin84@gmail.com>
2019-07-22 15:19:04 -0400

    installs axios and makes request to api and populates state on componentDidMount

--- src/containers/Movies.js
+++ src/containers/Movies.js
@@ -1,12 +1,30 @@
 import React, { Component } from 'react'
+import axios from 'axios'
 
 import Header from '../components/Header'
 
 class Movies extends Component {
+    state = {
+        movies: null
+    }
+
+    componentDidMount() {
+        axios.get('http://www.omdbapi.com/?s=Batman&apikey=48ba5f31')
+            .then ((res) => {
+                this.setState({
+                    movies: res.data.Search
+                })
+                console.log(this.state.movies)
+            })
+            .catch (err => {
+                console.log(err)
+            })
+    }
     render() {
         return (
             <div>
                 <Header/>
+                
             </div>
         )
     }
