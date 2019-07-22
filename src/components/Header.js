0ddbf18a673ca56088e05460ff005e75006e1904
Eric Mauldin <emauldin84@gmail.com>
2019-07-22 14:54:58 -0400

    first commit. adds components and containers files. adds bootstrap

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+let Header = () =>{
+    return(
+        <div className="container text-center">
+            <h1>React Batman Movies</h1>
+        </div>
+    )
+}
+
+export default Header
---
+++ src/containers/Movies.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+
+import Header from '../components/Header'
+
+class Movies extends Component {
+    render() {
+        return (
+            <div>
+                <Header/>
+            </div>
+        )
+    }
+}
+
+
+export default Movies
